814c3da26a1ecd379f1dc25321dbed1b1385ebc0
Rachit Tyagi <rachittyagi169@gmail.com>
2025-10-15 23:38:15 +0530

    started working on the testimonials section

---
+++ client/src/components/Home/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+     <div id='testimonial' className='flex flex-col items-center my-10 scroll-mt-12' >
+        
+     </div>
+  )
+}
+
+export default Testimonial
