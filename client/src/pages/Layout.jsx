bcb4708cfdf98e0cad2b07d6c22ec36708377555
Rachit Tyagi <rachittyagi169@gmail.com>
2025-11-01 16:11:21 +0530

    Completed navbar component and made some changes to layout section

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Layout = () => {
   return (
     <div>
-        <h1>layout</h1>
+      <Navbar/>
         <Outlet/>
+        
     </div>
   )
 }
